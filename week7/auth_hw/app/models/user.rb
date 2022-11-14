c7dd44cbe16ff7e3515d6a45456e8b7b28674b8f
Patrick Choi <ptrckchoi@gmail.com>
2022-11-13 23:19:05 -0800

    rails auth

---
+++ week7/auth_hw/app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true
+  validates :session_token, presence: true
+  validates :password_digest, presence: { message: "Password can't be blank" }
+
+end
