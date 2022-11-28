ded9dacfbc5982759de19eeab84898a92723ef4d
Patrick Choi <ptrckchoi@gmail.com>
2022-11-27 23:49:02 -0800

    w9d1hw

---
+++ week9/canvas_practice/canvas_practice/practice.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){
+  const canvasEl = document.getElementById("mycanvas");
+  canvasEl.width = 500;
+  canvasEl.height = 500;
+  const ctx = canvasEl.getContext('2d')
+});
